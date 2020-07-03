d6c96875a4149b86c49eb73a9ba96c5a0e77dcf0
shivaluma <shivaluma@gmail.com>
2020-07-03 13:02:07 +0700

    Upload project

---
+++ workers/app/app.js
@@ -0,0 +1,140 @@
+const fs = require('fs');
+const amqp = require('amqp-connection-manager');
+const redis = require('redis');
+const bail = require('bail');
+const rimraf = require('rimraf');
+
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379,
+});
+
+client.on('error', (err) => {
+  console.log('Error ' + err);
+});
+
+const extensions = {
+  cpp: 'cpp',
+  c: 'c',
+  java: 'java',
+  python3: 'py',
+};
+
+function runCode(apiBody, ch, msg) {
+  client.set(apiBody.folder.toString(), '{"status":"Processing"}');
+  const { exec } = require('child_process');
+  var output;
+  var command =
+    'python3 run.py ../temp/' +
+    apiBody.folder +
+    '/source.' +
+    extensions[apiBody.lang] +
+    ' ' +
+    apiBody.lang +
+    ' ' +
+    apiBody.timeOut;
+
+  fs.writeFile('/temp/' + apiBody.folder + '/output.txt', '', function (err) {
+    if (err) console.log(err);
+  });
+
+  fs.writeFile(
+    '/temp/' + apiBody.folder + '/expected_result.txt',
+    apiBody.expected_result,
+    function (err) {
+      if (err) console.log(err);
+    }
+  );
+
+  exec(command, (err, stdout, stderr) => {
+    if (err) {
+      ch.ack(msg);
+      console.log(err);
+    } else {
+      if (err) console.log(err);
+      var time = stdout.trim().split('\n');
+      var status = time.pop();
+      output = [];
+      if (status == 'Wrong Answer') {
+        output = time.pop().split(' ');
+      }
+      let result = {
+        output: output[1],
+        expected_answer: output[0],
+        time_used: time,
+        stderr: `${stderr}`,
+        status: `${status}`,
+        submission_id: apiBody.folder,
+      };
+
+      console.log(result);
+      console.log(apiBody);
+      rimraf('../temp/' + apiBody.folder, function (err) {
+        if (err) console.log(err);
+      });
+
+      client.setex(apiBody.folder.toString(), 3600, JSON.stringify(result));
+      ch.ack(msg);
+    }
+  });
+}
+
+function createFiles(apiBody, ch, msg) {
+  fs.mkdir('/temp/' + apiBody.folder, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      fs.writeFile(
+        '/temp/' + apiBody.folder + '/input.txt',
+        apiBody.input,
+        function (err) {
+          if (err) return console.log(err);
+          else {
+            fs.writeFile(
+              '/temp/' + apiBody.folder + '/source.' + extensions[apiBody.lang],
+              apiBody.src,
+              function (err) {
+                if (err) console.log(err);
+                else {
+                  runCode(apiBody, ch, msg);
+                }
+              }
+            );
+          }
+        }
+      );
+    }
+  });
+}
+
+const QUEUE_NAME = 'judge';
+var onMessage = function (data) {
+  let message = JSON.parse(data.content.toString());
+  createFiles(message, channelWrapper, data);
+};
+
+// Create a connetion manager
+var connection = amqp.connect(['amqp://rabbitmq:5672']);
+
+connection.on('connect', function () {
+  console.log('Connected!');
+});
+connection.on('disconnect', function (err) {
+  console.log('Disconnected.', err.stack);
+});
+
+// Set up a channel listening for messages in the queue.
+var channelWrapper = connection.createChannel({
+  setup: function (channel) {
+    // `channel` here is a regular amqplib `ConfirmChannel`.
+    return Promise.all([
+      channel.assertQueue(QUEUE_NAME, { durable: true }),
+      channel.prefetch(1),
+      channel.consume(QUEUE_NAME, onMessage),
+    ]);
+  },
+});
+
+channelWrapper.waitForConnect().then(function () {
+  console.log('Listening for messages');
+});
---
+++ workers/app/run.py
@@ -0,0 +1,91 @@
+import os
+import filecmp
+import sys
+from time import time
+import uuid
+import subprocess
+
+codes = {200: 'Accepted', 404: 'Server Error',
+         405: 'Wrong Answer',
+         406: 'Compile Error',
+         400: 'Runtime Error', 408: 'Time Limit Exceeded', 999: 'Wrong Answer'}
+
+
+def compile(file, lang):
+
+    if(lang == 'python3'):
+        return 200
+
+    if (os.path.isfile(file)):
+        if lang == 'c':
+            os.system('gcc -x c -Wall -O2 -static -pipe ' + file + ' -lm')
+        elif lang == 'cpp':
+            os.system(
+                'g++ -x c++ -std=c++14 -Wall -O2 -static -pipe -fomit-frame-pointer ' + file + ' -lm')
+        elif lang == 'java':
+            os.system('javac -encoding UTF-8 -sourcepath . -d . ' + file)
+        if (os.path.isfile('a.out')) or (os.path.isfile('main.class')):
+            return 200
+        else:
+            return 406
+    else:
+        return 404
+
+
+def run(file, input, timeout, lang):
+    cmd = 'sudo -u judge '
+    if lang == 'java':
+        cmd += 'java main'
+    elif lang == 'c' or lang == 'cpp':
+        cmd += './a.out'
+    elif lang == 'python3':
+        cmd += 'python3 ' + file
+    testcases = open(input, 'r').read().split("|||")
+    expected_results = open(expected_result, 'r').read().split("|||")
+
+    filename = "current_result"
+    for i in range(len(testcases)):
+        starttime = time()
+        result = ""
+        try:
+            result = subprocess.check_output("timeout {} {}".format(
+                timeout, cmd), universal_newlines=True, input=testcases[i], shell=True)
+        except subprocess.CalledProcessError as e:
+            if (e.returncode == 124):
+                return 408
+            return 400
+        endtime = time()
+        print((endtime-starttime)*1000)
+        if (expected_results[i].strip() != result):
+            print(result, expected_results[i])
+            return 999
+    return 200
+
+
+def match(output):
+    if os.path.isfile('out.txt') and os.path.isfile(output):
+        b = filecmp.cmp('out.txt', output)
+        os.remove('out.txt')
+        return b
+    else:
+        return 404
+
+
+params = sys.argv
+file = params[1].split('/')[3]
+path = os.getcwd()
+folder = params[1].split('/')[2]
+path = '../temp/' + folder + '/'
+
+os.chdir(path)
+lang = params[2]
+timeout = str(min(15, int(params[3])))
+
+
+testin = "input.txt"
+testout = "output.txt"
+expected_result = "expected_result.txt"
+status = compile(file, lang)
+if status == 200:
+    status = run(file, testin, timeout, lang)
+print(codes[status])
